e13f17cc32eef8460fff731af2d158d0d0fc0930
unknown <yh650766@gmail.com>
2020-06-01 23:32:16 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Box(){
+            List<object> emptyList = new List<object>();
+            emptyList.Add(7);
+            emptyList.Add(28);
+            emptyList.Add(-1);
+            emptyList.Add(true);
+            emptyList.Add("chair");
+            int sum = 0;
+            foreach (var objects in emptyList)
+            {
+                Console.WriteLine(objects);
+                if (objects is int)
+                {
+                    sum += (int)objects;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+        static void Main(string[] args)
+        {
+            Box();
+        }
+    }
+}
